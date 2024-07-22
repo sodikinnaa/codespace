71d46f5c2aea1940909fc09f394e677ff0917e4e
sodikinnaa <80803209+sodikinnaa@users.noreply.github.com>
2024-07-22 21:44:34 +0000

    setup wsgi

--- MasterApp/MasterApp/wsgi.py
+++ MasterApp/MasterApp/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MasterApp.settings')
 
 application = get_wsgi_application()
+app = get_wsgi_application()
